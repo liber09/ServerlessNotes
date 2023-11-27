8af9f485729842f33d7ec71b22952e8296cfe695
liber09 <linda.bergsangel@hotmail.com>
2023-11-27 04:47:01 +0100

    Can now retrieve deleted notes

---
+++ functions/notes/trash/index.js
@@ -0,0 +1,53 @@
+const { sendResponse } = require("../../../responses");
+const AWS = require('aws-sdk');
+const db = new AWS.DynamoDB.DocumentClient();
+const { validateToken } = require('../../middleware/auth');
+const middy = require('@middy/core');
+
+
+
+
+const getTrash = async(event,context)=>{
+
+    if (event?.error && event?.error === '401'){
+        return sendResponse(401, { success: false, message: 'invalid token' });
+    }
+        
+    const userName = event.queryStringParameters && event.queryStringParameters.userName;
+    if (!userName) {
+        return sendResponse(400, { success: false, message: 'userName parameter is missing in the query string' });
+    } 
+
+    const params = {
+        TableName: 'notesDb',
+        IndexName: 'UserNameIndex',
+        FilterExpression: '#userName = :userName AND #isDeleted = :isDeleted',
+        ExpressionAttributeNames: {
+            '#userName': 'userName',
+            '#isDeleted': 'isDeleted',
+        },
+        ExpressionAttributeValues: {
+            ':userName': userName,  
+            ':isDeleted': true,    
+        },
+    };
+
+    try {
+        const data = await db.scan(params).promise(); 
+        const notes = data.Items.map(item => ({
+            noteId: item.noteId,
+            title: item.title,
+            text: item.text,
+            createdAt: item.createdAt,
+            modifiedAt: item.modifiedAt,
+            userName: item.userName
+        }));
+        return sendResponse(200, { success: true, notes: notes });
+    } catch (err) {
+        return sendResponse(400, { success: false, message: "Unable to fetch data", error: JSON.stringify(err, null, 2) });
+    }
+};
+
+const handler = middy(getTrash).use(validateToken);
+
+module.exports = { handler };
